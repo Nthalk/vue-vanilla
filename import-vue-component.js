9dc9a9bc1fcce8ad503a56882a2ba8d384cbf5a7
Carl Taylor <carl@etaylor.me>
2017-12-16 22:53:14 -0800

    Fixed bugs, added better gh-pages

--- import-vue-component.js
+++ import-vue-component.js
@@ -1,39 +1,22 @@
 // MIT License Copyright (c) 2017 Carl Taylor
 var importVueComponent = (function () {
-    var r = "importVueComponent requires ";
-    if (!fetch) throw new Error(r + "fetch api polyfill (https://github.com/github/fetch)");
-    if (!Promise) throw new Error(r + "Promise api or polyfill (https://github.com/Ziriax/Promistix)");
-    if (!Vue) throw new Error(r + "Vue (https://unpkg.com/vue)");
+    var rf;
+    if (!fetch) rf = "fetch api polyfill (https://github.com/github/fetch)";
+    if (!Promise) rf = "Promise api or polyfill (https://github.com/Ziriax/Promistix)";
+    if (!Vue) rf = "Vue (https://unpkg.com/vue)";
+    if (rf) throw new Error("importVueComponent requires " + rf);
+
+    var w = window;
+    var d = document;
 
     var active = 0;
     var statusListeners = [];
+    var loadedCss = [];
+    var jsDepPromises = {};
 
     function statusReport() {
-        for (var i = 0; i < statusListeners.length; i++) {
-            if (active === 1) {
-                statusListeners[i](true);
-            } else if (active === 0) {
-                statusListeners[i](false);
-            }
-        }
-    }
-
-    function resolveLocation(type, location) {
-        if (!location || location.constructor !== String) {
-            console.error("Could not load:", type, location);
-        } else if (ivc.locations[location]) {
-            return ivc.locations[location];
-        } else if (location.startsWith("//") || location.startsWith("http")) {
-            return location;
-        } else {
-            if (ivc.prefixes[type] !== void 0) location = ivc.prefixes[type] + location;
-            if (ivc.suffixes[type] !== void 0) location = location + ivc.suffixes[type];
-            if (location.startsWith("$basePath")) {
-                if (ivc.basePath === void 0) throw new Error("loadVueComponent.basePath or contextPath is not set. Cannot determine where to load components from.");
-                location = ivc.basePath + location.substr("$basePath".length);
-            }
-        }
-        return location;
+        var isActive = active === 1;
+        if (isActive || active === 0) for (var i = 0; i < statusListeners.length; i++) statusListeners[i](isActive);
     }
 
     function finalize(resolve, obj, template, name, jsName) {
@@ -41,67 +24,8 @@ var importVueComponent = (function () {
         if (!obj.name) obj.name = name;
         obj.template = template;
         //console.log("finalizing", name);
-        window[jsName] = obj;
+        w[jsName] = obj;
         resolve(obj);
-    }
-
-    var jsDepPromises = {};
-
-    function handleAsyncComponent(resolve, obj, template, name, jsName) {
-        if (obj.require) {
-            //console.log("loading deps for", name);
-            var promises = [];
-            for (var jsDepName in obj.require) {
-                if (obj.require.hasOwnProperty(jsDepName)) {
-                    var depJsLocation = obj.require[jsDepName];
-                    if (window[jsDepName]) {
-                        //console.log("already load ed", jsDepName);
-                        promises.push(window[jsDepName]);
-                    } else if (jsDepPromises[jsDepName] !== void 0) {
-                        //console.log("reusing-loader", jsDepName);
-                        promises.push(jsDepPromises[jsDepName]);
-                    } else {
-                        //console.log("loading", jsDepName);
-                        jsDepPromises[jsDepName] = function (src) {
-                            return new Promise(function (resolve, reject) {
-                                if (src.lastIndexOf(".js") === src.length - 3) {
-                                    var script = document.createElement('script');
-                                    script.onload = resolve;
-                                    script.onerror = reject;
-                                    script.async = true;
-                                    script.src = src;
-                                    document.body.appendChild(script);
-                                } else {
-                                    var link = document.createElement('link');
-                                    link.rel = 'stylesheet';
-                                    link.type = 'text/css';
-                                    link.href = src;
-                                    link.media = 'all';
-                                    document.body.appendChild(link);
-                                    resolve();
-                                }
-                            });
-                        }(resolveLocation("lib", depJsLocation));
-                        promises.push(jsDepPromises[jsDepName]);
-                    }
-                }
-            }
-            Promise.all(promises).then(function () {
-                //console.log("finished loading deps for", name);
-                for (var jsDepName in obj.require) {
-                    if (obj.require.hasOwnProperty(jsDepName)) {
-                        delete jsDepPromises[jsDepName];
-                    }
-                }
-                if (obj.then && obj.then.constructor === Function) {
-                    finalize(resolve, obj.then(), template, name, jsName);
-                } else {
-                    finalize(resolve, obj, template, name, jsName);
-                }
-            });
-        } else {
-            finalize(resolve, obj, template, name, jsName);
-        }
     }
 
     function ivc(name, location, jsName) {
@@ -109,24 +33,23 @@ var importVueComponent = (function () {
         jsName = jsName || name;
 
         // We may have already loaded this
-        if (window[jsName] && (window[jsName].constructor instanceof Function || window[jsName] instanceof Object)) {
+        if (w[jsName] && (w[jsName].constructor === Function || w[jsName].constructor === Object)) {
             //console.log("Reusing loading definition", name);
-            return window[jsName];
+            return w[jsName];
         }
 
         // console.log("Loading definition", name);
-        var resolve;
-        var reject;
+        var resolve, reject;
         var promise = new Promise(function (_resolve, _reject) {
             resolve = _resolve;
             reject = _reject
         });
 
         function retrieve() {
-            fetch(resolveLocation("vue", location)).then(function (rsp) {
+            fetch(ivc.location(location, "vue")).then(function (rsp) {
                 return rsp.text();
             }).then(function (content) {
-                var div = document.createElement('div');
+                var div = d.createElement('div');
                 div.innerHTML = content;
                 var template = '';
                 var children = Array.prototype.slice.call(div.children);
@@ -134,24 +57,76 @@ var importVueComponent = (function () {
                 for (var i = 0; i < children.length; i++) {
                     var child = children[i];
                     if (child.tagName === 'STYLE') {
-                        document.head.appendChild(child);
+                        d.head.appendChild(child);
                     } else if (child.tagName === 'SCRIPT') {
                         eval(child.innerText);
-                        obj = window[jsName];
-                        window[jsName] = promise;
+                        obj = w[jsName];
+                        w[jsName] = promise;
                     } else if (child.outerHTML) {
-                        if (template) console.error("Vue component " + name + " should wrap it's template in one element");
+                        if (template) throw new Error("Vue component " + name + " should wrap it's template in one element");
                         template = child.outerHTML;
                     }
                 }
-                if (!template) console.error("Vue component " + name + " should wrap it's template in one element");
-                handleAsyncComponent(resolve, obj, template, name, jsName);
+
+                if (!template) throw new Error("Vue component " + name + " should wrap it's template in one element");
+                if (!obj) throw new Error("loadVueComponent component" + jsName + " is not defined after loading component, did you forget to set the definition on the global scope? e.g. window['" + jsName + "'] = {}")
+
+                if (obj.require) {
+                    //console.log("loading deps for", name);
+                    var promises = [];
+                    for (var jsDepName in obj.require) {
+                        if (!obj.require.hasOwnProperty(jsDepName)) continue;
+                        var depJsLocation = obj.require[jsDepName];
+                        if (w[jsDepName]) {
+                            //console.log("already load ed", jsDepName);
+                            promises.push(w[jsDepName]);
+                        } else if (jsDepPromises[jsDepName] !== void 0) {
+                            //console.log("reusing-loader", jsDepName);
+                            promises.push(jsDepPromises[jsDepName]);
+                        } else {
+                            //console.log("loading", jsDepName);
+                            jsDepPromises[jsDepName] = function (src) {
+                                return new Promise(function (resolve, reject) {
+                                    if (src.lastIndexOf(".js") === src.length - 3) {
+                                        var script = d.createElement('script');
+                                        script.onload = resolve;
+                                        script.onerror = reject;
+                                        script.async = true;
+                                        script.src = src;
+                                        d.head.appendChild(script);
+                                    } else {
+                                        if (loadedCss.indexOf(src) === -1) {
+                                            loadedCss.push(src);
+                                            var link = d.createElement('link');
+                                            link.rel = 'stylesheet';
+                                            link.type = 'text/css';
+                                            link.href = src;
+                                            link.media = 'all';
+                                            d.head.appendChild(link);
+                                        }
+                                        // Auto resolve css
+                                        resolve();
+                                    }
+                                });
+                            }(ivc.location(depJsLocation, "lib"));
+                            promises.push(jsDepPromises[jsDepName]);
+                        }
+                    }
+                    Promise.all(promises).then(function () {
+                        //console.log("finished loading deps for", name);
+                        for (var jsDepName in obj.require) if (obj.require.hasOwnProperty(jsDepName)) delete jsDepPromises[jsDepName];
+                        if (obj.then) obj = obj.then.constructor === Function ? obj.then() : obj.then;
+                        finalize(resolve, obj, template, name, jsName);
+                    });
+                } else {
+                    finalize(resolve, obj, template, name, jsName);
+                }
             });
         }
 
         if (ivc.eager) retrieve();
 
-        Vue.component(name, window[jsName] = function (resolve) {
+        Vue.component(name, w[jsName] = function (resolve) {
             if (!ivc.eager) retrieve();
             active++;
             statusReport();
@@ -159,23 +134,51 @@ var importVueComponent = (function () {
                 if (--active === 0) statusReport();
             });
         });
-        return window[jsName];
+        return w[jsName];
     }
 
-    ivc.eager = true;
-    ivc.locations = {};
-    ivc.prefixes = {
+    /**
+     * Define how to resolve a location given a type
+     */
+    var r = ivc.location = function (location, type) {
+        if (!location || location.constructor !== String) throw new Error("Could not load:" + type + " resource at " + location);
+        if (r.overrides[location]) return r.overrides[location];
+        if (location.indexOf("//") === 0 || location.indexOf("http") === 0) return location;
+        if (r.prefixes[type] !== void 0) location = r.prefixes[type] + location;
+        if (r.suffixes[type] !== void 0) location = location + r.suffixes[type];
+        if (location.indexOf("$basePath") === 0) {
+            if (r.basePath === void 0) throw new Error("loadVueComponent.basePath or contextPath is not set. Cannot determine where to load components from.");
+            location = r.basePath + location.substr("$basePath".length);
+        }
+        return location;
+    };
+    r.overrides = {};
+    r.prefixes = {
         vue: "$basePath/vue/",
         lib: "$basePath/vendor/"
     };
-    ivc.suffixes = {
+    r.suffixes = {
         vue: ".vue.html"
     };
-    ivc.basePath = "";
+    r.basePath = "";
+
+    ivc.eager = false;
 
     ivc.track = function (statusListener) {
         statusListeners.push(statusListener);
         statusListener(active !== 0);
+    };
+    ivc.onceLoaded = function (cb) {
+        if (active === 0) {
+            cb();
+            return;
+        }
+        var a;
+        statusListeners.push(a = function (loaded) {
+            if (!loaded) return;
+            cb();
+            statusListeners.splice(statusListeners.indexOf(a), 1);
+        })
     };
 
     return ivc;
