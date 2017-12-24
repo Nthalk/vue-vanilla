341189a2538e289cd0483234acc3b80ee12b88ac
Carl Taylor <carl@etaylor.me>
2017-12-23 20:52:34 -0800

    Regen min/map

--- dist/import-vue-component.js
+++ dist/import-vue-component.js
@@ -1,5 +1,5 @@
 // MIT License Copyright (c) 2017 Carl Taylor,
-// Version: 1.1.1-SNAPSHOT
+// Version: 1.1.2-SNAPSHOT
 var importVueComponent = (function () {
     var n = "importVueComponent ";
     var e;
